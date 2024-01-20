c653070bc4ae3e7140007f0c9b7d80ff4de9ab55
Your Name <abb.iheb112@gmail.com>
2024-01-20 17:47:56 +0100

    Initial Commit

---
+++ src/main/java/com/wedyor/coachiny/CoachinyApplication.java
@@ -0,0 +1,13 @@
+package com.wedyor.coachiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachinyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedyor/coachiny/CoachinyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedyor.coachiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachinyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
